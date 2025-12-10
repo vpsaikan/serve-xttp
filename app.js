0af4fa5a0f98740e8f0450f39b0311f43948e433
vpsaikan <wuwangguoshang89@gmail.com>
2025-12-10 18:31:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const NEZHA_KEY = process.env.NEZHA_KEY || '';             // v1的NZ_CLIENT_SEC
 const AUTO_ACCESS = process.env.AUTO_ACCESS || true;      // 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量
 const XPATH = process.env.XPATH || UUID.slice(0, 8);       // xhttp路径,自动获取uuid前8位
 const SUB_PATH = process.env.SUB_PATH || 'sub';            // 节点订阅路径
-const DOMAIN = process.env.DOMAIN || 'tealsailfish.onpella.app';                   // 域名或ip,留空将自动获取服务器ip
+const DOMAIN = process.env.DOMAIN || 'hugface.qiudao.dpdns.org';                   // 域名或ip,留空将自动获取服务器ip
 const NAME = process.env.NAME || 'pella';                       // 节点名称
 const PORT = process.env.PORT || 3000;                     // http服务
 
